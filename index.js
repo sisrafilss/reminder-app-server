32e689d716a16b03defe613c95a3ef99f37ed05e
Israfil Hossen <sisrafilss@gmail.com>
2023-04-24 07:36:02 +0600

    project initalized

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.quv1r.mongodb.net:27017,cluster0-shard-00-01.quv1r.mongodb.net:27017,cluster0-shard-00-02.quv1r.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-teugro-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("reminder_app");
+    const remindersCollection = database.collection("reminders");
+
+    app.get("/all-reminders", async (req, res) => {
+      const cursor = remindersCollection.find({});
+      const reminders = await cursor.toArray();
+
+      res.json(reminders);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Simple ExpressJS server is running...");
+});
+
+app.listen(port, () => {
+  console.log("Server has started at port", port);
+});
